e2eb2ab071c06e60eb402ee0fb294c41d02ba7b9
Dhananjay Singh <dhananjay1204@gmail.com>
2018-08-04 13:28:00 +0530

    composite pattern

---
+++ src/mynotes/patterns/structural/composite/IComponent.java
@@ -0,0 +1,9 @@
+package mynotes.patterns.structural.composite;
+
+public interface IComponent {
+	String name="asdsad";
+	
+	void play();
+	void setPlaybackSpeed(float speed);
+	String getName();
+}
---
+++ src/mynotes/patterns/structural/composite/Playlist.java
@@ -0,0 +1,37 @@
+package mynotes.patterns.structural.composite;
+
+import java.util.ArrayList;
+
+public class Playlist implements IComponent {
+
+	public String playlistName;
+	public ArrayList<IComponent> playlist = new ArrayList();
+
+	public Playlist(String playlistName) {
+		this.playlistName = playlistName;
+	}
+
+	public void add(IComponent component) {
+   		 playlist.add(component);
+}
+
+	public void remove(IComponent component) {
+		playlist.remove(component);
+	}
+	
+	public void play(){
+		for(IComponent component : playlist) {
+			component.play();
+	    	}
+	}
+
+	public void setPlaybackSpeed(float speed) {
+	      for(IComponent component : this.playlist){
+		component.setPlaybackSpeed(speed);
+	    }
+	}
+
+	public String getName() {
+	    return this.playlistName;
+	}
+}
