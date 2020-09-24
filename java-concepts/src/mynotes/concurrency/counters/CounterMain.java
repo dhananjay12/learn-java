4397aff97660ea69d8c79ea0efd1a74f943159f2
dhananjay12 <dhananjay1204@gmail.com>
2020-09-24 15:39:34 +0200

    counters

--- java-concepts/src/mynotes/concurrency/counters/CounterMain.java
+++ java-concepts/src/mynotes/concurrency/counters/CounterMain.java
@@ -8,7 +8,7 @@ public class CounterMain {
     public static final int REPS = 10_000_000;
 
     public static void main(String[] args) throws InterruptedException {
-        final Counter c = new AtomicCounter();
+        final Counter c = new VolatileCounter();
 
         Runnable r = () -> {
             for (int i = 0; i < REPS; i++) {
